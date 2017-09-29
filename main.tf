da0b57b8420b1c159aa08a0cadf45c7e57e3556f
Alasdair Lumsden <1268988+alaslums@users.noreply.github.com>
2017-09-29 19:54:20 +0100

    Adding IPv6 Support

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "label" {
 }
 
 module "subnets" {
-  source             = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.2.3"
+  source             = "git::https://github.com/everycity/terraform-aws-dynamic-subnets.git"
   availability_zones = "${var.availability_zones}"
   namespace          = "${var.namespace}"
   name               = "${var.name}"
@@ -19,6 +19,7 @@ module "subnets" {
   vpc_id             = "${aws_vpc.default.id}"
   cidr_block         = "${aws_vpc.default.cidr_block}"
   igw_id             = "${aws_internet_gateway.default.id}"
+  eoigw_id           = "${aws_egress_only_internet_gateway.default.id}"
   delimiter          = "${var.delimiter}"
   attributes         = ["${compact(concat(var.attributes, list("subnets")))}"]
   tags               = "${var.tags}"
@@ -27,10 +28,16 @@ module "subnets" {
 resource "aws_vpc" "default" {
   cidr_block           = "${var.cidr_block}"
   enable_dns_hostnames = true
+  enable_dns_support   = true
+  assign_generated_ipv6_cidr_block = true
   tags                 = "${module.label.tags}"
 }
 
 resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
   tags   = "${module.label.tags}"
+}
+
+resource "aws_egress_only_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
 }
